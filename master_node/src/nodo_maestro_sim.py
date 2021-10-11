a1b27931976fe94829de5526ca2e90d68bfb0cf1
Miguel <miguel.sdguez@gmail.com>
2021-10-11 19:08:01 +0200

    Now the simulation changes the max_update_rate by itself. Default value set to 100

--- master_node/src/nodo_maestro_sim.py
+++ master_node/src/nodo_maestro_sim.py
@@ -3,17 +3,26 @@ from sys import path
 import roslaunch
 import rospy
 from eufs_msgs.msg import CarState, CanState
+from gazebo_msgs.msg import ODEPhysics
+from gazebo_msgs.srv import GetPhysicsProperties, SetPhysicsProperties
 
 import os
 import re
+
+MAX_UPDATE_RATE = 100
 
 class Master_node(object):
     def __init__(self):
         self.state_sub = rospy.Subscriber('/ros_can/state', CanState, self.callbackstate)
 
+        self.gazebo_initialized = False
+
         self.state = 'OFF'
 
     def callbackstate(self, msg):
+        if not self.gazebo_initialized:
+            self.gazebo_initialized = True
+
         if msg.ami_state == 10:
             self.state = 'OFF'
         elif msg.ami_state == 11:
@@ -42,6 +51,7 @@ if __name__ == "__main__":
     uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
     roslaunch.configure_logging(uuid)
 
+    # Relative path obtention
     current_path = os.getcwd()
 
     launch_path = None
@@ -54,6 +64,21 @@ if __name__ == "__main__":
     master_node = Master_node()
     rospy.init_node('master_node', anonymous=True)
     rospy.loginfo("started")
+
+    # Wait until gazebo initializes
+    while not master_node.gazebo_initialized:
+        rospy.sleep(0.1)
+
+    # Get the current physics properties and change the max_update_rate
+    try:
+        gazebo_get_physics_properties = rospy.ServiceProxy("gazebo/get_physics_properties", GetPhysicsProperties)
+        gazebo_set_physics_properties = rospy.ServiceProxy("gazebo/set_physics_properties", SetPhysicsProperties)
+        properties = gazebo_get_physics_properties()
+
+        response = gazebo_set_physics_properties(properties.time_step, MAX_UPDATE_RATE, properties.gravity, properties.ode_config)
+    except rospy.ServiceException as exc:
+        print("Service did not process request: " + str(exc))
+
 
     while master_node.state == 'OFF':
         rospy.sleep(0.1)
