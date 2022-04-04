8126a05c23108c9350e04a555babc7ba754a5b2a
anunem <alfredo.nunezm@alumnos.upm.es>
2022-04-04 19:04:35 +0200

    add speed profile for simulations

--- control_node/src/Algoritmo_Stanley.py
+++ control_node/src/Algoritmo_Stanley.py
@@ -4,6 +4,8 @@ from cmath import sqrt
 import numpy as np
 import math
 import scipy.interpolate as scipy_interpolate
+import scipy.signal as scipe_signal
+from scipy.signal import _peak_finding
 
 
 Kp = 1.0                                    # speed proportional gain
@@ -11,7 +13,7 @@ ki = 1.0                                    # speed integral gain
 kd = 0.1                                    # speed derivational gain
 dt = 0.1                                    # [s] time difference
 target_speed = 10.0 / 3.6                   # [m/s]
-Ke = 5                                      # control gain
+Ke = 6                                      # control gain
 Kv = 1
 max_steer = 27.2 * np.pi / 180              # [rad] max steering angle
 max_accel = 1.0                             #[m/s^2] max desacceleration
@@ -19,7 +21,7 @@ mu=0.8                                      #coef ade
 max_desaccel=mu*9.8*0.5                          # [m/s^2] max desacceleration 0.5 safety factor 
 vuelta_reconomiento=0
 dist_inicio=6
-target_vuelta=1
+target_vuelta=3
 indice_prev=0
 
 VEL_THRESHOLD = 1e-4
@@ -57,6 +59,7 @@ class Stanley(object):
         positions_x = []
         positions_y = []
         global vuelta_reconomiento
+        global target_speed
 
 
 
@@ -72,8 +75,17 @@ class Stanley(object):
             path_x, path_y = self.interpolate_b_spline_path(positions_x, positions_y, self.n_course_point, len(positions_x) - 1)
 
         if 'path_x' in locals():
-            self.stanley_control(path_x, path_y)
+            current_target=self.stanley_control(path_x, path_y)
+            
+            if vuelta_reconomiento==1 and self.finish_flag==0:
+                VelProf=self.speed_profile(path_x, path_y)
+                target_speed=VelProf[current_target]
+                print("target speed =" ,target_speed)
+            
+            if self.finish_flag==1:
+                target_speed=0
             self.pid_control(target_speed)
+            print("Actual speed =" ,self.v)
 
         if self.finish_flag==1 and self.v<VEL_THRESHOLD     :
             print("mision status =finish ")
@@ -81,6 +93,193 @@ class Stanley(object):
         if (positions_x[0]==positions_x[-1] and  positions_y[0]==positions_y[-1]) and vuelta_reconomiento==0:
             print ("vuelta de reconocimiento ")
             vuelta_reconomiento=1
+
+    def speed_profile(self,x,y):
+
+        """
+        INPUT DATA
+        % name = 'name_of_track'
+        % m = mass
+        % ftmax = max traction (N)
+        % fbmax = max braking (N)
+        % fnmax = max cornering (N)
+        % trackData - [x-ref y-ref xin yin xout yout]
+        % Values I had taken
+        % m = 100, ftmax = 16, fbmax = -18, fnmax = 30
+
+        % OUTPUT DATA
+        % velProf = velocity profile of the given track
+        """
+        m = 100
+        ftmax =3
+        fbmax = -5
+        fnmax = 8 
+        n=len(x)
+        ftmax = ftmax*m # traction max
+        fbmax = -fbmax*m # braking max
+        fnmax = fnmax*m # cornering max
+        drag = 0.0021*m # drag
+        accel=np.zeros(n)
+        decel=np.zeros(n)
+        velProf=np.zeros(n)
+        R=np.zeros(n)
+        K=np.zeros(n)
+        
+        #segment length 
+        lon = np.zeros(n)
+        
+        for i in range(1,n):
+            lon[i]=lon[i-1]+np.sqrt((x[i]-x[i-1])**2+(y[i]-y[i-1])**2)
+
+
+        # funci0n curvatura 
+        R=np.zeros(n)
+        for i in range(1,n-1):
+            A=np.array([x[i-1] ,y[i-1], 0.0])
+            B=np.array([x[i] ,y[i], 0])    
+            C=np.array([x[i+1] ,y[i+1], 0.0])
+            
+            D=np.array(np.cross((B-A),(C-A)))
+            b=np.linalg.norm(A-C)
+            c=np.linalg.norm(A-B)
+            a=np.linalg.norm(B-C)
+            R[i] = a*b*c/2/np.linalg.norm(D)
+            # if i==0:
+            #     R[0]=R[1]
+
+                        
+                        
+            if (R[i]-R[i-1])>1 and i>2 :
+                R[i]=R[i-1]+1
+
+
+            
+            
+
+
+        
+
+
+
+
+        R[0]=R[1]
+        R[n-1]=R[n-2]
+        # print("R[]=%f" %     R[1])
+
+
+
+        
+
+        for i in range(n):
+        #    print("R[]=%f" %     R[i])
+            K[i]=1/R[i]
+        #    print("K[]=%f" %     K[i])
+
+
+        print("%d" % len(R))
+        print("%d" % len(x))
+        print("%d" % len(y))
+
+        peaks, _ = scipe_signal.find_peaks(K)
+        print("%d" % len(peaks))
+
+        # for i in range(len(peaks)):
+        #     print("peak[]=%f" %     peaks[i])
+        
+        ## start section 
+        accel[0]=0
+        if len(peaks)>1:
+            for i in range(1,peaks[1]+1):
+                accel[i]=np.sqrt(2*np.sqrt((ftmax)**2-((m*K[i-1]*accel[i-1]**2)**2*(ftmax/fnmax)**2))*(lon[i]-lon[i-1])/m + accel[i-1]**2)
+
+            for i in range(len(peaks)):
+                #check if accel vel is greater than crit vel
+                vcrit = np.sqrt(fnmax/(m*K[peaks[i]]))
+                if accel[peaks[i]] <vcrit:
+                    curVel=accel[peaks[i]]
+                else:
+                    curVel=vcrit
+                    accel[peaks[i]]=vcrit
+                #next step 
+                
+                accel[peaks[i]+1]=np.sqrt(2*np.sqrt((ftmax)**2-((m*K[peaks[i]]*curVel**2)**2*(ftmax/fnmax)**2))*(lon[peaks[i]]-lon[peaks[i]-1])/m + curVel**2)
+
+                if i==len(peaks)-1:
+                    #end sec
+                    for j in range (peaks[i]+2,n):
+                        accel[j]=np.sqrt(2*np.sqrt((ftmax)**2-((m*K[j-1]*accel[i-1]**2)**2*(ftmax/fnmax)**2))*(lon[j]-lon[j-1])/m + accel[j-1]**2)
+                        #accel[j]=np.sqrt(2*np.sqrt((ftmax-drag*accel[i-1]**2)**2-(m*K[j-1]*((ftmax-drag*accel[i-1]**2)/fnmax)**2*accel[j-1]**2)**2)*(lon[j]-lon[j-1])/m + accel[j-1]**2)
+                else:
+                    #usual step 
+                    for j in range (peaks[i]+2,peaks[i+1]+1):
+                        #accel[j]=np.sqrt(2*np.sqrt((ftmax-drag*accel[j-1]**2)**2-(m*K[j-1]*((ftmax-drag*accel[j-1]**2)/fnmax)**2*accel[j-1]**2)**2)*(lon[j]-lon[j-1])/m + accel[j-1]**2)
+                        accel[j]=np.sqrt(2*np.sqrt((ftmax)**2-((m*K[j-1]*accel[j-1]**2)**2*(ftmax/fnmax)**2))*(lon[j]-lon[j-1])/m + accel[j-1]**2)
+        
+
+        #end point
+        decel[-1]=20
+        if len(peaks)>1:
+            for j in range (len(decel)-2,peaks[-1]-1,-1):
+                decel[j]=np.sqrt(2*np.sqrt((fbmax)**2-((m*K[j+1]*decel[j+1]**2)**2*(fbmax/fnmax)**2))*(lon[j+1]-lon[j])/m + decel[j+1]**2)
+            
+            
+
+            print(len(peaks))
+            for i in range(len(peaks)-1,0,-1):    
+                #check decel 
+                vcrit = np.sqrt(1*fnmax/(m*K[peaks[i]]))
+                if decel[peaks[i]]<vcrit:
+                    curVel=decel[peaks[i]]
+                else:
+                    curVel=vcrit
+                    decel[peaks[i]]=vcrit
+                #next step
+                decel[peaks[i]-1]=np.sqrt(2*np.sqrt((fbmax)**2-((m*K[peaks[i]]*curVel**2)**2*(fbmax/fnmax)**2))*(lon[peaks[i]]-lon[peaks[i]-1])/m + curVel**2)
+                
+                
+                if i==1:
+                    #start
+                    for j in range (peaks[i]-2,0,-1):
+                        decel[j]=np.sqrt(2*np.sqrt((fbmax)**2-((m*K[j+1]*decel[j+1]**2)**2*(fbmax/fnmax)**2))*(lon[j+1]-lon[j])/m + decel[j+1]**2)
+                        
+                        
+                    #usual step
+                else:
+                    for j in range(peaks[i]-2,peaks[i-1]-1,-1):
+                        decel[j]=np.sqrt(2*np.sqrt((fbmax)**2-((m*K[j+1]*decel[j+1]**2)**2*(fbmax/fnmax)**2))*(lon[j+1]-lon[j])/m + decel[j+1]**2)
+                    
+        
+        if (len(peaks)==1):
+            for i in range(n):
+                velProf[i]=v
+        else:
+            velProf=np.minimum(accel,decel)
+        
+        
+
+        # print("%d" % len(velProf))
+        # for i in range (len(velProf)):
+        #     print("V[]",i, velProf[i])
+
+
+
+        
+        # plt.plot(x,y)
+        # plt.show()
+        # fig, ax1 = plt.subplots()
+        # plt.plot(velProf) 
+        # ax1.plot(velProf, color="gray")
+        # ax2 = ax1.twinx() 
+        # ax2.plot(K)
+        # plt.plot(peaks, K[peaks], "x")
+        # plt.plot(np.zeros_like(x), "--", color="gray")
+        # plt.show()
+
+
+
+
+        return velProf
+
 
        
 
@@ -164,6 +363,7 @@ class Stanley(object):
         # print("error_cuadrado: {}".format(self.error_cuadrado))
         # print("suma error cuadrado: {}".format(sum(self.error_cuadrado)))
         # print("RMS: {}".format(self.rmse_acumulado))
+        return current_target_idx
 
     def calc_target_spline(self, path_x, path_y):
         """
@@ -195,7 +395,7 @@ class Stanley(object):
             self.flag_entrada_circulo=0
 
         if self.contador_de_vuelta==target_vuelta:
-            target_speed=0
+            
             self.finish_flag=1
         indice_prev=min_idx
 
